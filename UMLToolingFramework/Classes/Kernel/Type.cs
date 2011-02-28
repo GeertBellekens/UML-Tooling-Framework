321d3e08a031acbecaa9caacdb82bacbf9e89028
Geert Bellekens <geert@bellekens.com>
2011-02-28 06:13:42 +0100

    added navigate to using attributes function

--- UMLToolingFramework/Classes/Kernel/Type.cs
+++ UMLToolingFramework/Classes/Kernel/Type.cs
@@ -1,3 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using UML=TSF.UmlToolingFramework.UML;
+
+
 namespace TSF.UmlToolingFramework.UML.Classes.Kernel {
-	public interface  Type : PackageableElement {}
+	public interface  Type : PackageableElement {
+		
+		HashSet<UML.Classes.Kernel.Property> getUsingAttributes();
+		HashSet<UML.Classes.Kernel.Parameter> getUsingOperations();
+	}
+	
 }
