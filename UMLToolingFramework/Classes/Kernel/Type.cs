98f1e5f8fd59710f12c2b7fd4b5618c6c37bd301
Geert Bellekens <geert@bellekens.com>
2011-03-02 05:51:50 +0100

    added some documentation

--- UMLToolingFramework/Classes/Kernel/Type.cs
+++ UMLToolingFramework/Classes/Kernel/Type.cs
@@ -9,7 +9,11 @@ using UML=TSF.UmlToolingFramework.UML;
 namespace TSF.UmlToolingFramework.UML.Classes.Kernel {
 	public interface  Type : PackageableElement {
 		
-		
+		/// <summary>
+		/// Returns the TypedElements that use this Type as their type.
+		/// Results are limited to the given subtype of TypedElement
+		/// </summary>
+		/// <returns>The TypedElements that use this Type as their type</returns>
 		HashSet<T> getDependentTypedElements<T>() where T:UML.Classes.Kernel.TypedElement;
 	}
 	
