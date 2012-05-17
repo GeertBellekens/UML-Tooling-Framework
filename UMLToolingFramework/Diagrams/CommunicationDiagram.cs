9ca6cec20af6062f135db6325f892841369d516f
Geert Bellekens <geert@bellekens.com>
2012-05-17 14:25:55 +0200

    added support for CommunicationDiagrams

---
+++ UMLToolingFramework/Diagrams/CommunicationDiagram.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+using TSF.UmlToolingFramework;
+
+namespace TSF.UmlToolingFramework.UML.Diagrams {
+  public interface CommunicationDiagram:Diagram {
+
+  	
+	List<UML.Classes.Kernel.Operation> getCalledOperations();
+  }
+}
