114de07c6e378119f16a66aca4acfbb191f137cb
Geert Bellekens <geert@bellekens.com>
2016-04-05 08:23:06 +0200

    update for multiple subset associations

--- SchemaBuilderFramework/SchemaAssociation.cs
+++ SchemaBuilderFramework/SchemaAssociation.cs
@@ -12,14 +12,14 @@ namespace SchemaBuilderFramework
 	public interface SchemaAssociation
 	{
 		UML.Classes.Kernel.Association sourceAssociation {get;set;}
-		UML.Classes.Kernel.Association subsetAssociation {get;set;}
+		List<UML.Classes.Kernel.Association> subsetAssociations {get;set;}
 		List<SchemaElement> relatedElements {get;set;}
 		SchemaElement owner {get;set;}
 		/// <summary>
 		/// the schema element on the other side of the association
 		/// </summary>
 		SchemaElement otherElement {get;set;}
-		UML.Classes.Kernel.Association createSubsetAssociation();
+		void createSubsetAssociation();
 		/// <summary>
 		/// returns the end of the schema association's source association that is on the owner side
 		/// </summary>
