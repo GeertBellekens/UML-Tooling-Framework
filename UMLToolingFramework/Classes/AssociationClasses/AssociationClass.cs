35c1a266e195f422324a3924b69ecf13923d060c
Geert Bellekens <geert@bellekens.com>
2016-05-08 10:01:28 +0200

    added support for AssociationClasses

---
+++ UMLToolingFramework/Classes/AssociationClasses/AssociationClass.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+
+namespace TSF.UmlToolingFramework.UML.Classes.AssociationClasses
+{
+	/// <summary>
+	/// A model element that has both Association and Class properties. An AssociationClass can be seen as an Association that
+	/// also has Class properties, or as a Class that also has Association properties. It not only connects a set of Classifiers but
+	/// also defines a set of Features that belong to the Association itself and not to any of the associated Classifiers.
+	/// </summary>
+	public interface AssociationClass : TSF.UmlToolingFramework.UML.Classes.Kernel.Class, TSF.UmlToolingFramework.UML.Classes.Kernel.Association
+	{
+		
+	}
+}
