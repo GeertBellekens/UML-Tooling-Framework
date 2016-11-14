922dd1f84289a2fa5a63b555328e4cb034df3412
Geert Bellekens <geert@bellekens.com>
2016-11-14 08:59:59 +0100

    new WorkitemTracking project

---
+++ WorkTrackingFramework/Project.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace WorkTrackingFramework
+{
+	/// <summary>
+	/// Description of Project.
+	/// </summary>
+	public interface Project
+	{
+		List<Workitem> workitems {get;set;}
+	}
+}
---
+++ WorkTrackingFramework/Workitem.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace WorkTrackingFramework
+{
+	/// <summary>
+	/// Description of MyClass.
+	/// </summary>
+	public interface Workitem
+	{
+		string ID {get;set;}
+		string type {get;set;}
+		string title {get;set;}
+		string state {get;set;}
+		string description {get;set;}
+		string assignedTo {get;set;}
+		string area {get;set;}
+		string iteration {get;set;}
+	}
+}
