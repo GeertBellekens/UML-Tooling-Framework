e260322ed3f404dc3f59e738e28152bcaf21dce5
Christophe VG <contact@christophe.vg>
2017-01-13 16:45:26 +0100

    added Index representation

---
+++ DatabaseFramework/Index.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace DatabaseFramework
+{
+	/// <summary>
+	/// Description of Index.
+	/// </summary>
+	public interface Index:Constraint
+	{
+	}
+}
