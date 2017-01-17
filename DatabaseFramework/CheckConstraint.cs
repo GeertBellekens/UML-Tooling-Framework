34212083f099526034b3b61422a1d2dac7050db6
Geert Bellekens <geert@bellekens.com>
2017-01-17 14:17:19 +0100

    added checkconstraint

---
+++ DatabaseFramework/CheckConstraint.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+
+
+namespace DatabaseFramework
+{
+	/// <summary>
+	/// Description of CheckConstraint.
+	/// </summary>
+	public interface CheckConstraint:Constraint
+	{
+		string rule {get;set;}
+	}
+}
