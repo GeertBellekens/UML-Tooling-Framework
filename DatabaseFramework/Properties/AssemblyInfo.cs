361e86d4db193aa2c9861d7d556ba3a3e030b2f4
Geert Bellekens <geert@bellekens.com>
2018-10-11 09:07:17 +0200

    fixed project structure

--- DatabaseFramework/Properties/AssemblyInfo.cs
+++ DatabaseFramework/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 
 // This sets the default COM visibility of types in the assembly to invisible.
 // If you need to expose a type to COM, use [ComVisible(true)] on that type.
-[assembly: ComVisible(true)]
+[assembly: ComVisible(false)]
 
 // The assembly version has following format :
 //
