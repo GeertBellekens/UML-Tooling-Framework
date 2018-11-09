fab79eaead94bd9354f22563be3faca90f12c525
Geert Bellekens <geert@bellekens.com>
2018-11-09 14:27:02 +0100

    formatting

--- UMLToolingFramework/Classes/Kernel/Class.cs
+++ UMLToolingFramework/Classes/Kernel/Class.cs
@@ -1,11 +1,13 @@
 using System.Collections.Generic;
 
-namespace TSF.UmlToolingFramework.UML.Classes.Kernel {
-  public interface Class : Classifier {
-		HashSet<Property>   ownedAttributes   { get; set; }
-		HashSet<Classifier> nestedClassifiers { get; set; }
-		HashSet<Operation>  ownedOperations   { get; set; }
-    	HashSet<Class>      superClasses      { get; set; }
-    	HashSet<Class> 	subClasses {get;}
-	}
+namespace TSF.UmlToolingFramework.UML.Classes.Kernel
+{
+    public interface Class : Classifier
+    {
+        HashSet<Property> ownedAttributes { get; set; }
+        HashSet<Classifier> nestedClassifiers { get; set; }
+        HashSet<Operation> ownedOperations { get; set; }
+        HashSet<Classifier> superClasses { get; set; }
+        HashSet<Classifier> subClasses { get; }
+    }
 }
