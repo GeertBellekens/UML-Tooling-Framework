7340567395a86c0c2ca43aa06503f02b7b2eedbd
Geert Bellekens <geert@bellekens.com>
2022-06-14 15:36:41 +0200

    added profileName to stereotype

--- UMLToolingFramework/Profiles/Stereotype.cs
+++ UMLToolingFramework/Profiles/Stereotype.cs
@@ -4,5 +4,8 @@ namespace TSF.UmlToolingFramework.UML.Profiles {
     /// According to the UML specs the Stereotype is not really a 
     /// NamedElement, but for the sake of simplicity we have let it derive
     /// from NamedElement. Still to be seen if that is a wise choice.
-    public interface Stereotype : UML.Classes.Kernel.NamedElement {}
+    public interface Stereotype : UML.Classes.Kernel.NamedElement 
+    {
+        string profileName { get; set; }
+    }
 }
